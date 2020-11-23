67eaa2f7a0d7f4c70109836f630e2dd98d556c81
Nacho Caiafa <nachokai@outlook.com>
2020-11-22 22:35:51 -0300

    add pug engine

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
-
+require("dotenv").config();
+const express = require("express");
+const myDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
+const port = process.env.PORT || 3000;
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+fccTesting(app);
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
-  res.render('Load your view here');
+app.route("/").get((req, res) => {
+  res.render("Load your view here");
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Listening on port ' + process.env.PORT);
+app.listen(port, () => {
+  console.log("Listening on port " + port);
 });
