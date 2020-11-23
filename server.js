077dd69fdc55f29b36c691a60f47d5896e56f3ac
Nacho Caiafa <nachokai@outlook.com>
2020-11-22 22:43:37 -0300

    add pug engine

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "pug");
 
 app.route("/").get((req, res) => {
-  res.render("index");
+  res.render("views/pug");
 });
 
 app.listen(port, () => {
